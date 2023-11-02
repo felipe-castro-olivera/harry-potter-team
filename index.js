5d81b7067941c4c99c155f5268841fc4e481471e
sailiid <neymarsaime@gmail.com>
2023-11-02 13:48:07 -0500

    agregue rama

---
+++ index.js
@@ -0,0 +1,11 @@
+const urlApi = "https://hp-api.onrender.com/api/characters"
+
+const { createApp } = Vue;
+
+      createApp({
+        data() {
+          return {
+            message: "Hola Harry!",
+          };
+        },
+      }).mount("#app");
