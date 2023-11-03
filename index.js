1a1c987295dbe59200d2d11b1f4162fe9abeb904
sailiid <neymarsaime@gmail.com>
2023-11-02 19:30:23 -0500

    Realice el filtro de checkbox

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const urlApi = "https://hp-api.onrender.com/api/characters"
+const urlAPI = "https://hp-api.onrender.com/api/characters"
 
 const { createApp } = Vue;
 
@@ -6,6 +6,42 @@ const { createApp } = Vue;
         data() {
           return {
             message: "Hola Harry!",
+            personajes: null,
+            personajesBkp: [],
+            categoriasCasas: [],
+            categoriasSeleccionadas:[],
+            buscador: "", 
+
+    
           };
+        },
+        mounted(){
+
+        },
+        created(){
+            this.dataHarry(urlAPI)
+
+        },
+        methods:{
+            dataHarry(url){
+                fetch(url).then(response => response.json()).then(data => {
+                    this.personajes = data
+                    this.personajesBkp = data
+                    this.categoriasCasas = Array.from(new Set(this.personajes.map((personaje) => personaje.house).splice(4,4)))
+                    console.log(this.categoriasCasas);
+                })
+            }
+
+        },
+        computed:{
+            filtro(){
+                let filtrobuscador = this.personajesBkp.filter(personaje.name.toLowerCase().includes(this.buscador.toLowerCase()))
+
+                if(this.categoriasSeleccionadas.length == 0){
+                    this.personajes = filtrobuscador
+                }else{
+                    this.personajes = filtrobuscador.filter(personaje => this.categoriasSeleccionadas.includes(personaje.house))
+                }
+            },
         },
       }).mount("#app");
